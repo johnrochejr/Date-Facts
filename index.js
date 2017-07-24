85e76ff2ae7125fbbf697add2fc68be0049d12f1
John Roche <roche.johnjr@gmail.com>
2017-07-24 17:53:21 -0400

    Complete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dateNow = moment().format("LLLL");
 const dayOfYear = moment().format("DDD");
 const seconds = moment().startOf('day');
 const dls = moment().isDST();
+const leapYear = moment().isLeapYear();
 
 console.log(chalk.blue('Hello world!'));
 
@@ -13,8 +14,16 @@ console.log(`It is the ${chalk.magenta(dayOfYear + 'th')} day of the year.`);
 
 console.log(`It is ${chalk.yellow(moment().diff(seconds, 'seconds'))} seconds into the day.`);
 
+// DLS
 if (dls) {
   console.log('It ' + chalk.green('is') + ' during Daylight Savings Time.');
 } else {
   console.log('It ' + chalk.green('is not') + ' during Daylight Savings Time.');
+}
+
+// Leap Year
+if (leapYear) {
+  console.log(`It ${chalk.yellow('is')} a leap year.`);
+} else {
+  console.log(`It ${chalk.red('is not')} a leap year.`);
 }
