66290b538f8da079095b708a7109d84904415c5a
John Roche <roche.johnjr@gmail.com>
2017-07-24 17:45:50 -0400

    working through strings

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-require('./app/index') 
+const chalk = require('chalk');
+const moment = require('moment');
+const dateNow = moment().format("LLLL");
+const dayOfYear = moment().format("DDD");
+const seconds = moment().startOf('day');
+const dls = moment().isDST();
+
+console.log(chalk.blue('Hello world!'));
+
+console.log(`It is ${chalk.blue(dateNow)}.`);
+
+console.log(`It is the ${chalk.magenta(dayOfYear + 'th')} day of the year.`);
+
+console.log(`It is ${chalk.yellow(moment().diff(seconds, 'seconds'))} seconds into the day.`);
+
+if (dls) {
+  console.log('It ' + chalk.green('is') + ' during Daylight Savings Time.');
+} else {
+  console.log('It ' + chalk.green('is not') + ' during Daylight Savings Time.');
+}
